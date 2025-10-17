2d66b4c634fc7cc37b0093a0daee28b1fb319f88
Denys Vuika <denys.vuika@gmail.com>
2025-10-17 15:06:25 +0100

    Update app.config.ts documentation to clarify that the dynamic endpoint can be overridden by a plugin

--- src/api/app.config.ts
+++ src/api/app.config.ts
@@ -3,7 +3,7 @@ import { PluginConfig } from "./types";
 
 /**
  * App Config API plugin
- * Provides a dynamic app.config.json endpoint
+ * Provides an example dynamic app.config.json endpoint that can be overridden by a plugin
  */
 export default function (app: Express, config: PluginConfig) {
   app.get("/app.config.json", (req: Request, res: Response) => {
